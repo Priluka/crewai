fe4e7e51ac63e138174618d20e395ab0fdc0afdb
ivanduvnjak <ivan.duvnjak8@gmail.com>
2025-05-29 12:38:59 +0200

    remove on tool start

--- src/crewai/agents/tools_handler.py
+++ src/crewai/agents/tools_handler.py
@@ -37,7 +37,7 @@ class ToolsHandler:
                 output=output,
             )
 
-remo    # def on_tool_start(self, tool_name: str):
+    # def on_tool_start(self, tool_name: str):
     #   self.tool_chunkId = str(uuid.uuid4())
     #   self.socket_io.send_to_socket(
     #     text=f"Using tool: {tool_name.capitalize()}",
